4649380e856978a747dac8fe2c480c8d5b21da5c
Simone Kamer <simone.kamer@gmail.com>
2017-12-04 21:23:12 +0100

    finds the message by returning the most found character on each position

---
+++ src/main/java/demo/day6/CharacterCounter.java
@@ -0,0 +1,30 @@
+package demo.day6;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class CharacterCounter {
+    Map<Character, Integer> countedCharacterMap = new HashMap<>();
+
+    void countEveryCharacter(Character letter) {
+
+        if (countedCharacterMap.containsKey(letter)) {
+            Integer timesFound = countedCharacterMap.get(letter);
+            countedCharacterMap.put(letter, (timesFound + 1));
+        } else {
+            countedCharacterMap.put(letter, 1);
+        }
+    }
+
+    public Character getMostCommunicatedCharacter() {
+        Character mostFoundCharacter = '\0';
+        int maxValueInMap=(Collections.max(countedCharacterMap.values()));
+        for (Map.Entry<Character, Integer> entry : countedCharacterMap.entrySet()) {
+            if (entry.getValue()==maxValueInMap) {
+                mostFoundCharacter = (entry.getKey()); 
+            }
+        }
+        return mostFoundCharacter;
+    }
+}
---
+++ src/test/java/demo/day6/RepeatingMessageRecovererTest.java
@@ -0,0 +1,32 @@
+package demo.day6;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class RepeatingMessageRecovererTest {
+
+    RepeatingMessageRecoverer recoverer = new RepeatingMessageRecoverer();
+
+    @Test
+    public void ItShouldRecoverEaster() throws Exception {
+        String message = "eedadn\n" +
+                "drvtee\n" +
+                "eandsr\n" +
+                "raavrd\n" +
+                "atevrs\n" +
+                "tsrnev\n" +
+                "sdttsa\n" +
+                "rasrtv\n" +
+                "nssdts\n" +
+                "ntnada\n" +
+                "svetve\n" +
+                "tesnvt\n" +
+                "vntsnd\n" +
+                "vrdear\n" +
+                "dvrsen\n" +
+                "enarar";
+        assertThat(recoverer.recoverMessage(message), is("easter"));
+    }
+}
